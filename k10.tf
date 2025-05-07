98b20a3aa3cc56a5981501965586297be17b34d7
privaltr <rick@ricksdomein.nl>
2025-05-07 14:33:24 +0200

    k10 istio

--- k10.tf
+++ k10.tf
@@ -12,24 +12,8 @@ YAML
   ]
 }
 
-# Kasten K10 TLS Module (for secure communication)
-module "k10_tls" {
-  count     = var.enable_k10 ? 1 : 0
-  source    = "./modules/tls-cert"
-  namespace = var.k10_namespace
-  dns_names = [
-    "k10.${var.base_domain}"
-  ]
-  certs_path = var.certs_path
-
-  depends_on = [
-    kind_cluster.default,
-    helm_release.cert_manager,
-  ]
-}
-
 # Kasten K10 Helm Release Deployment
-resource "helm_release" "kasten_k10_deployment" {
+resource "helm_release" "k10_deployment" {
   count            = var.enable_k10 ? 1 : 0
   name             = "k10"
   repository       = "https://charts.kasten.io"
@@ -51,43 +35,95 @@ resource "helm_release" "kasten_k10_deployment" {
     value = "50Gi"
   }
 
+  # Configure snapshot support
+  set {
+    name  = "global.snapshot.enabled"
+    value = "true"
+  }
+
   depends_on = [
     kind_cluster.default,
-    kubectl_manifest.k10_namespace,
-    module.k10_tls,
+    helm_release.cert_manager,
+    # module.k10_tls,
   ]
 }
 
-# Kasten K10 Ingress using ProjectContour HTTPProxy
-resource "kubectl_manifest" "k10_ingress" {
+resource "kubectl_manifest" "k10_istio_gateway" {
   count     = var.enable_k10 ? 1 : 0
   yaml_body = <<YAML
-apiVersion: projectcontour.io/v1
-kind: HTTPProxy
+apiVersion: networking.istio.io/v1beta1
+kind: Gateway
 metadata:
-  annotations:
-    kubernetes.io/ingress.class: contour
-  name: kasten-ingress
+  name: k10-gateway
   namespace: ${var.k10_namespace}
 spec:
-  virtualhost:
-    fqdn: k10.${var.base_domain}
-    tls:
-      secretName: ${module.k10_tls[0].cert_secret}
-  routes:
-    - conditions:
-        - prefix: /
-      enableWebsockets: true  # Critical for Kasten
-      services:
-        - name: gateway       # Correct service name
-          port: 80           # Matches service definition
-      pathRewritePolicy:
-        replacePrefix:
-        - replacement: /k10/
+  selector:
+    istio: ingressgateway  # This matches the Istio ingress gateway service
+  servers:
+    - port:
+        number: 443
+        name: https
+        protocol: HTTPS
+      tls:
+        mode: SIMPLE
+        credentialName: k10-https-cert  # Must exist in the same namespace as istio ingress gateway (default is istio-system)
+      hosts:
+        - k10.${var.base_domain}
 YAML
   depends_on = [
     kind_cluster.default,
-    helm_release.kasten_k10_deployment,
-    module.k10_tls,
+    helm_release.k10_deployment,
+    helm_release.cert_manager,
+    helm_release.istio_ingress,
+    kubectl_manifest.k10-certificate
+  ]
+}
+resource "kubectl_manifest" "k10_virtualservice" {
+  count     = var.enable_k10 ? 1 : 0
+  yaml_body = <<YAML
+apiVersion: networking.istio.io/v1beta1
+kind: VirtualService
+metadata:
+  name: k10
+  namespace: ${var.k10_namespace}
+spec:
+  hosts:
+    - k10.${var.base_domain}
+  gateways:
+    - k10-gateway
+  http:
+    - match:
+        - uri:
+            prefix: /
+      route:
+        - destination:
+            host: gateway.${var.k10_namespace}.svc.cluster.local
+            port:
+              number: 80
+YAML
+  depends_on = [
+    kubectl_manifest.k10_istio_gateway,
+  ]
+}
+
+resource "kubectl_manifest" "k10-certificate" {
+  count     = var.enable_k10 ? 1 : 0
+  yaml_body = <<YAML
+apiVersion: cert-manager.io/v1
+kind: Certificate
+metadata:
+  name: k10-tls-certificate
+  namespace: istio-system
+spec:
+  secretName: k10-https-cert
+  dnsNames:
+    - "k10.${var.base_domain}"
+  issuerRef:
+    name: root-ca-issuer
+    kind: ClusterIssuer
+    group: cert-manager.io  # REQUIRED field
+YAML
+  depends_on = [
+    kubectl_manifest.root_ca_issuer,
   ]
 }
